13dc6cb29823d3853081717525fe917b1044ab6a
onlinity <pavle.ignjatovic.08@gmail.com>
2024-01-03 12:04:21 +0100

    Added gallery

---
+++ gallery/imagelink.js
@@ -0,0 +1,17 @@
+
+document.getElementById('gallery').querySelectorAll('img').forEach(element => {
+    
+    let text = element.outerHTML;
+    let src = text;
+    src = src.slice(src.indexOf('"') + 1);
+    src = src.slice(0, src.indexOf('"'));
+    
+    var link = document.createElement('a');
+    link.innerHTML = text;
+    link.href = src;
+    link.target = "_blank"
+    
+    
+    element.parentNode.insertBefore(link, element);
+    element.remove();
+});
